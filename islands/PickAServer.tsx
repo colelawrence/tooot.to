db1dfed6a83592f0e33db4f312ae7e0cca12af0c
Cole Lawrence <cole@colelawrence.com>
2022-11-19 13:56:27 -0500

    Enable custom server endpoint specification

--- islands/PickAServer.tsx
+++ islands/PickAServer.tsx
@@ -2,7 +2,7 @@ import ServerLaunchButton from "../components/ServerLaunchButton.tsx";
 import servers from "../SERVERS.gen.json" assert { type: "json" };
 
 interface PickAServerProps {
-  cta?: string,
+  cta?: string;
   previousServerUsed?: string | undefined;
   recommendedServers: string[];
 }
@@ -16,27 +16,50 @@ export default function PickAServer(props: PickAServerProps) {
           {launchButtonContainerJSX(props, props.previousServerUsed)}
           <details>
             <summary class="font-bold text-base">Other common servers</summary>
-            <div
-              style={{
-                // fix gaps between inline blocks
-                fontSize: `0px`,
-              }}
-            >
-              {servers.map((s) => launchButtonContainerJSX(props, s))}
-            </div>
+            {bigListOfServers(props)}
           </details>
         </div>
       ) : (
-        <div
-          style={{
-            // fix gaps between inline blocks
-            fontSize: `0px`,
-          }}
-        >
-          {servers.map((s) => launchButtonContainerJSX(props, s))}
-        </div>
+        bigListOfServers(props)
       )}
     </div>
+  );
+}
+
+function bigListOfServers(props: PickAServerProps) {
+  return (
+    <div>
+      <div
+        style={{
+          // fix gaps between inline blocks
+          fontSize: `0px`,
+        }}
+      >
+        {servers.map((s) => launchButtonContainerJSX(props, s))}
+      </div>
+      <h3 class="font-bold text-base">Other</h3>
+      {serverLaunchCustomInput({ value: props.previousServerUsed ?? "" })}
+    </div>
+  );
+}
+
+function serverLaunchCustomInput(props: { value: string }) {
+  return (
+    <form
+      method="POST"
+      class="flex px-1 h-12 justify-between items-center bg-white rounded-sm border border-black relative"
+    >
+      <input
+        name="value"
+        type="text"
+        value={props.value}
+        placeholder="myawesomeserver.com"
+        class="p-1 w-full"
+      />
+      <button type="submit" class="uppercase text-sm p-4">
+        Save
+      </button>
+    </form>
   );
 }
 
